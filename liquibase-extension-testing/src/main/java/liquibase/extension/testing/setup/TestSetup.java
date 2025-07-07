e76de285884f8a5dd41a8db4438ec1d485a7678d
shshdxk <609022045@qq.com>
2025-07-07 18:10:09 +0800

    删除测试代码

--- liquibase-extension-testing/src/main/java/liquibase/extension/testing/setup/TestSetup.java
+++
@@ -1,9 +0,0 @@
-package liquibase.extension.testing.setup;
-
-public abstract class TestSetup {
-
-    public abstract void setup(TestSetupEnvironment testSetupEnvironment) throws Exception;
-    public void cleanup() {
-
-    }
-}
